12f254e1c2065c58571a89a10a23d39d7d0d39f7
Sayali Patil <137858267+sayalii17@users.noreply.github.com>
2023-08-26 23:57:43 +0530

    Add files via upload

---
+++ cal_script.js
@@ -0,0 +1,26 @@
+let input= document.getElementById('ipBox');
+let btns= document.querySelectorAll('button');
+
+let str= "";
+let arr= Array.from(btns);
+arr.forEach(button => {
+	button.addEventListener('click', (e)=> {
+		if(e.target.innerHTML == "="){
+			str= eval(str);
+			input.value= str;
+		}
+		else if(e.target.innerHTML == 'AC'){
+			str="";
+			input.value=str;
+		}
+		else if(e.target.innerHTML == 'DEL'){
+			str= str.substring(0,str.length-1);
+			input.value=str;
+		}
+		else{
+			str += e.target.innerHTML;
+			input.value= str;
+		}
+		
+	})
+})
